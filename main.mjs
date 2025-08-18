fa5eb4ded20e1ec2827fb2d5d234d3e0feae4935
glsndroj <galsandooorj@gmail.com>
2025-08-18 12:50:42 +0800

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,22 @@
+// const fs = require("fs");
+import fs from "fs";
+// // Relative
+// fs.readFile("../Java/arrowfunction.html", "utf8", (err, data) => {
+//   console.log(data);
+// });
+
+// // Absolute
+// fs.readFile("../index.html", "utf8", (err, data) => {
+//   console.log(data);
+// });
+
+// // Write
+// fs.writeFileSync("../readme.txt", "hi hi", "utf-8");
+
+// Import( uur file dotor bgaa function import hj oruulj irj boldog, import {duudah function bichne hed ch bj boln} from './index.html(ene n import hiih file)')
+import { Numbers } from "../number.mjs";
+Numbers();
+
+// Export(uur file dotor bgaa function export hiine, function ii urd export)
+
+// npm - Node Package Manager
